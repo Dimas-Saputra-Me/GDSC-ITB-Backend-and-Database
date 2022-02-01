b8f8c1ecd76aeaa0e2068b71b06069279ead917b
Nicholas Sidharta <toxicancm@gmail.com>
2022-02-01 14:33:13 +0700

    cancel

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ require('dotenv').config();
 const routes = require('./backend/api');
 
 const app = express();
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+const PORT = process.env.PORT || 8080; // Step 1
 
 
 //mongodb
